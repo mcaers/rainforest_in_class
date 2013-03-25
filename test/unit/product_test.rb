d29f72934470144981d0e2e949c188bb6dc0d344
Khurram Virani <kvirani@gmail.com>
2013-03-25 11:04:09 -0400

    More product validation tests

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -16,4 +16,21 @@ class ProductTest < ActiveSupport::TestCase
     refute @p.valid?
   end
 
+  test "must have a name" do 
+    @p.name = ""
+    refute @p.valid?
+  end
+
+  test "price_in_cents must be a integer" do 
+    @p.price_in_cents = 1.5
+    assert !@p.valid?
+  end
+
+  test "must have a price_in_cents" do 
+    @p.price_in_cents = nil
+    refute @p.valid?
+  end
+
+
+
 end
