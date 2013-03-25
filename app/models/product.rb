bd7bcbbd33bbb708a3aa74284c8ca1bcd6052c8b
Khurram Virani <kvirani@gmail.com>
2013-03-25 11:04:34 -0400

    Show price in dollars (using helper) and set price in dollars (broken right now)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,17 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price_in_cents
+  attr_accessible :description, :name, :price_in_cents, :price_in_dollars
 
   validates :description, :name, :presence => true
   validates :price_in_cents, :numericality => {:only_integer => true}
+
+  def price_in_dollars
+    self.price_in_cents.to_f / 100
+  end
+
+  #product.price_in_dollars = 500.50
+  def price_in_dollars=(amount)
+    #amount #=> 6.42
+    self.price_in_cents = (amount * 100).to_i
+  end
+
 end
